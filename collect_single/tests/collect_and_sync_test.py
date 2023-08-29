d39dc44ee074f51caacf0f683d57cca9b786d914
Matthias Veit <matthias_veit@yahoo.de>
2023-08-29 10:48:06 +0200

    add tox.ini and remove invalid test

--- collect_single/tests/collect_and_sync_test.py
+++ collect_single/tests/collect_and_sync_test.py
@@ -1,24 +1,28 @@
 import asyncio
-from pytest import mark
+from typing import AsyncIterator
+
+from pytest import fixture
 from resotoclient.async_client import ResotoClient
 
 
-async def core_client() -> ResotoClient:
+@fixture
+async def core_client() -> AsyncIterator[ResotoClient]:
     client = ResotoClient("http://localhost:8980")
-    while True:
+    flag = True
+    while flag:
         try:
             await client.ping()
-            return client
+            flag = False
+            yield client
         except Exception:
             await asyncio.sleep(1)
 
 
-@mark.asyncio
-async def test_client() -> None:
-    client = await core_client()
-    configs = [
-        cfg.split("resoto.report.benchmark.", maxsplit=1)[-1]
-        async for cfg in client.cli_execute("configs list")
-        if cfg.startswith("resoto.report.benchmark.")
-    ]
-    print(configs)
+# @mark.asyncio
+# async def test_client(core_client: ResotoClient) -> None:
+#     configs = [
+#         cfg.split("resoto.report.benchmark.", maxsplit=1)[-1]
+#         async for cfg in core_client.cli_execute("configs list")
+#         if cfg.startswith("resoto.report.benchmark.")
+#     ]
+#     print(configs)
