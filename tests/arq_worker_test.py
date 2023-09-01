3eb2199d1e1a65250422de2a9b93a4c4553a25e3
Matthias Veit <matthias_veit@yahoo.de>
2023-09-01 12:04:19 +0200

    use os environment

--- tests/arq_worker_test.py
+++ tests/arq_worker_test.py
@@ -33,9 +33,9 @@ async def test_enqueue_job(arq_redis: ArqRedis) -> None:
         graphdb_password="",
         worker_config={"resotoworker": {"collector": ["aws"]}},
         env={
-            "AWS_ACCESS_KEY_ID": "",
-            "AWS_SECRET_ACCESS_KEY": "",
-            "AWS_SESSION_TOKEN": "",
+            "AWS_ACCESS_KEY_ID": os.environ["AWS_ACCESS_KEY_ID"],
+            "AWS_SECRET_ACCESS_KEY": os.environ["AWS_SECRET_ACCESS_KEY"],
+            "AWS_SESSION_TOKEN": os.environ["AWS_SESSION_TOKEN"]
         },
         account_len_hint=2,
     )
