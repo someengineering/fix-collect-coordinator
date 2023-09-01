34a79fe7f5922dc0dfd25c95545a971f5a4fc0fd
Matthias Veit <matthias_veit@yahoo.de>
2023-09-01 12:00:51 +0200

    only run redis tests if redis is running

--- tests/arq_worker_test.py
+++ tests/arq_worker_test.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from contextlib import suppress
 from typing import Dict, Any, List
 
@@ -48,6 +49,7 @@ async def test_enqueue_job(arq_redis: ArqRedis) -> None:
 
 
 @pytest.mark.asyncio
+@pytest.mark.skipif(os.environ.get("REDIS_RUNNING") is None, reason="Redis is not running")
 async def test_queue(arq_redis: ArqRedis) -> None:
     # This is the worker function that is called by arq.
     async def the_task(ctx: Dict[str, Any], *args: Any, **kwargs: Any) -> str:
