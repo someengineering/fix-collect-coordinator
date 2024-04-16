dd75ee03e1e17a34dd72633d5302e78d74b13921
Matthias Veit <matthias_veit@yahoo.de>
2024-04-16 10:50:27 +0200

    [feat] Add health check incl redis and k8s client

--- collect_coordinator/api.py
+++ collect_coordinator/api.py
@@ -13,23 +13,41 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+import prometheus_client
 from aiohttp.web import Request, Response, Application, get, json_response
 from cattr import unstructure
 from fixcloudutils.service import Service
-import prometheus_client
+from kubernetes_asyncio.client.api_client import ApiClient
+from kubernetes_asyncio import client as k8s
+from redis.asyncio import Redis
 
 from collect_coordinator.job_coordinator import KubernetesJobCoordinator
 
 
 class Api(Service):
-    def __init__(self, app: Application, coordinator: KubernetesJobCoordinator) -> None:
-        app.add_routes([get("/ping", self.ping), get("/status", self.status), get("/metrics", self.metrics)])
+    def __init__(
+        self, app: Application, coordinator: KubernetesJobCoordinator, redis: Redis, kubernetes_client: ApiClient
+    ) -> None:
+        app.add_routes(
+            [
+                get("/ping", self.ping),
+                get("/status", self.status),
+                get("/metrics", self.metrics),
+                get("/health", self.health),
+            ]
+        )
         self.app = app
         self.coordinator = coordinator
+        self.redis = redis
+        self.api_client = kubernetes_client
 
     async def ping(self, _: Request) -> Response:
         return Response(text="pong")
+
+    async def health(self, _: Request) -> Response:
+        await self.redis.ping()  # throws exception in case of error
+        await k8s.CoreV1Api(self.api_client).list_namespace(limit=1)  # throws exception in case of error
+        return Response(text="ok")
 
     async def status(self, _: Request) -> Response:
         return json_response({"queue": unstructure(self.coordinator.job_queue)})
